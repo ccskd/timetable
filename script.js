b5b905d91f336108c3ceca6bc43f008788ac5472
kaidai <156929926+ccskd@users.noreply.github.com>
2025-09-01 01:13:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function mapShow(venue){
+    show = document.getElementById("show");
+    if (venue == 'kklg'){
+        show.innerText = 'See map: 21';
+    }
+    else if (venue == 'cpd') {
+        show.innerText = 'See map: 35b';
+    }
+    else if (venue == 'wlgh') {
+        show.innerText = 'See map: 4b';
+    }
+
+}
